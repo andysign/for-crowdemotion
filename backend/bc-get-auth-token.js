2314317081e1af065317027cab67b491302764bc
andysign <andysignerdesigner@gmail.com>
2019-11-07 21:03:02 +0000

    Added the most important script designed to geth the authorization token from Dynata

---
+++ backend/bc-get-auth-token.js
@@ -0,0 +1,23 @@
+// Backend Get Authorization Token Test
+const request = require('request-promise');
+
+const prc = process;
+const password = prc.argv[2]?prc.argv[2]:(prc.env.PASSWORD?prc.env.PASSWORD:'');
+const url = 'https://api.uat.pe.researchnow.com/auth/v1/token/password';
+
+const options = { method: 'POST',
+	uri: url,
+	headers: { 'content-type': 'application/json' },
+	body: {
+		clientId: 'api',
+		password: password,
+		username: 'crowd_emotion_sample_api' },
+	json: true };
+
+const call = request(options);
+
+if (require.main !== module) module.exports = call;
+
+call.then( function(r) {
+	console.log(r.accessToken);
+} ).catch( function() { console.error('Call failed...'); } );
