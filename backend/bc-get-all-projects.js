b4e8c8a787f3beb82d590b08af8ac5eb2c03eecc
andysign <andysignerdesigner@gmail.com>
2019-11-08 21:44:22 +0000

    Added a way to get the auth from bc-get-auth-token js

--- backend/bc-get-all-projects.js
+++ backend/bc-get-all-projects.js
@@ -1,21 +1,31 @@
-// var request = require("request");
 const request = require("request-promise");
+// var request = require("request");
+const getAuthToken = require('./bc-get-auth-token');
 
 const prc = process;
-const auth = prc.argv[2] ? prc.argv[2] : (prc.env.AUTH?prc.env.AUTH:'');
+const auth = prc.argv[2] ? prc.argv[2] : '';
 const url = 'https://api.uat.pe.researchnow.com/sample/v1/projects';
 
 const options = { method: 'GET',
 	url: url,
 	qs: { limit: '10' },
 	headers:
-		{ authorization: 'Bearer '+auth,
+		{ authorization: '', // 'Bearer '+auth
 		'content-type': 'application/json' } };
 
-const call = request(options);
-
-if (require.main !== module) module.exports = call;
-
-call.then( function(r) {
-	console.log(r);
-} ).catch( function() { console.error('Call failed...'); } );
+if (auth != '') {
+	options.headers.authorization = 'Bearer '+auth;
+	const call = request(options);
+	call.then( function(r) {
+		console.log(r);
+	} ).catch( function() { console.error('Call failed...'); } );
+} else {
+	getAuthToken.then(function(a){
+		options.headers.authorization = 'Bearer '+a.accessToken;
+		const call = request(options);
+		if (require.main !== module) { module.exports = call; }
+		call.then( function(r) {
+			console.log(r);
+		} ).catch( function() { console.error('Call failed...'); } );
+	}).catch( function() { console.error('Error'); });
+}
