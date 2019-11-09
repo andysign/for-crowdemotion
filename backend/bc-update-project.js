f3da827b92758bcae728572ebf8a01d58ef54e28
andysign <andysignerdesigner@gmail.com>
2019-11-09 02:04:12 +0000

    Crafted again bc-update-prj js with promises

--- backend/bc-update-project.js
+++ backend/bc-update-project.js
@@ -1,32 +1,35 @@
 /*
 * Backend Update a Given Projects With Given Data
 * Usage: node bc-update-project.js 'project001' '{"title":"Text"}'
-* UsageAsModule: require('./bc-update-project')
+* UsageAsModule:require('./bc-update-project').updateProject().then(console.log)
+* UsageAsModuleAwait: await require('./bc-update-project').updateProject()
 */
-const request = require("request-promise");
-// var request = require("request");
+const request = require("request-promise"); // var request = require("request");
 const getAuthToken = require('./bc-get-auth-token');
+
+const updateProject = function (prj, body) {
+	prj = prj === undefined ? 'project001' : prj;
+	body = body === undefined ? '{"title":"Text"}' : body;
+	return new Promise( function (res, rej) {
+		require('./bc-get-auth-token').getAuth().then(function(auth){
+			const path= 'sample/v1/projects/';
+			const domain = 'https://api.uat.pe.researchnow.com/';
+			const url = domain + path + prj;
+			const options = { method: 'POST',
+							url: url,
+							headers:{ authorization: 'Bearer '+auth,
+									'content-type': 'application/json' },
+							body: JSON.parse(body),
+							json: true }
+			request(options).then(function(r){
+				res(r);
+			});
+		});
+	} );
+}
+
+module.exports = { updateProject: updateProject }
 
 const prj = process.argv[2] ? process.argv[2] : 'project001';
 const body= process.argv[3] ? process.argv[3] : '{"title":"Text"}';
-const path= 'sample/v1/projects/';
-const domain = 'https://api.uat.pe.researchnow.com/';
-// const url = 'https://api.uat.pe.researchnow.com/sample/v1/projects/project001';
-const url = domain + path + prj;
-
-var options = { method: 'POST',
-	url: url,
-	headers:
-		{ authorization: '',//'Bearer '+auth,
-			'content-type': 'application/json' },
-	body: JSON.parse(body),
-	json: true };
-
-getAuthToken.then(function(a){
-	options.headers.authorization = 'Bearer '+a.accessToken;
-	const call = request(options);
-	if (require.main !== module) { module.exports = call; }
-	call.then( function(r) {
-		console.log(r);
-	} ).catch( function() { console.error('Call failed...'); } );
-}).catch( function() { console.error('Error'); });
+if (require.main === module) updateProject(prj, body).then(console.log);
