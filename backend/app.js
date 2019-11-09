3b8e0bdb46b9f91d6d29c17151fccea8ce52b6d9
andysign <andysignerdesigner@gmail.com>
2019-11-09 12:02:04 +0000

    Added error handler

--- backend/app.js
+++ backend/app.js
@@ -27,4 +27,15 @@ app.use(function(req, res, next) {
 	next(createError(404));// res.send('Page Not Found\n 404!\n');
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = process.env.NODE_ENV === 'development' ? err : '|';
+	// render the error page
+	res.status(err.status || 500);
+	// console.log(res.locals);
+	res.send('ERROR:'+err.status+':'+res.locals.error);
+});
+
 module.exports = app;
