14f63a4ac06936ea1565c08cac94e2c7fd974035
andysign <andysignerdesigner@gmail.com>
2019-11-09 16:33:40 +0000

    Added get all countries bc script

---
+++ backend/bc-get-all-countries.js
@@ -0,0 +1,23 @@
+const request = require("request-promise"); // var request = require("request");
+const getAuthToken = require('./bc-get-auth-token');
+
+const getAllCountries = function (prj, body) {
+	return new Promise( function (res, rej) {
+		getAuthToken.getAuth().then(function(auth){
+			const url='https://api.uat.pe.researchnow.com/sample/v1/countries';
+			const options = { method: 'GET',
+				url: url,
+				qs: { limit: '10' },
+				headers:
+					{	authorization: 'Bearer '+auth+'',
+						'content-type': 'application/json' } };
+			request(options).then(function(r){
+				res(r);
+			});
+		});
+	} );
+}
+
+module.exports = { getAllCountries: getAllCountries }
+
+if (require.main === module) getAllCountries().then(console.log);
