46354d048569459d8848730142dfa2d115b35809
andysign <andysignerdesigner@gmail.com>
2019-11-09 02:03:39 +0000

    Crafted again get auth token js , this time with promises

--- backend/bc-get-auth-token.js
+++ backend/bc-get-auth-token.js
@@ -1,28 +1,29 @@
 /*
 * Backend Get Authorization Token Test
 * Usage: node bc-get-auth-token.js [password]
-* UsageAsModule: require('./bc-get-auth-token')
+* UsageAsModule: require('./bc-get-auth-token').getAuth().then(console.log)
+* UsageAsModuleAwait: await require('./bc-get-auth-token').getAuth()
 */
 const request = require('request-promise');
 
-const p = process;
-const password=require.main===module?(p.argv[2]||p.env.PASSWORD):p.env.PASSWORD;
-const url = 'https://api.uat.pe.researchnow.com/auth/v1/token/password';
-
-const options = { method: 'POST',
-	url: url,
-	headers: { 'content-type': 'application/json' },
-	body: {
-		clientId: 'api',
-		password: password,
-		username: 'crowd_emotion_sample_api' },
-	json: true };
-
-const call = request(options);
-
-if (require.main !== module) { module.exports = call; }
-
-if (require.main === module) {
-	call.then( function(r) { console.log(r.accessToken); } )
-		.catch( function() { console.error('Call failed...'); } );
+const getAuth = function () {
+	return new Promise( function (res, rej) {
+		const password = process.env.PASSWORD;
+		const url = 'https://api.uat.pe.researchnow.com/auth/v1/token/password';
+		const options = { method: 'POST',
+			url: url,
+			headers: { 'content-type': 'application/json' },
+			body: {
+				clientId: 'api',
+				password: password,
+				username: 'crowd_emotion_sample_api' },
+			json: true };
+		request(options).then(function(r){
+			res(r.accessToken);
+		});
+	} );
 }
+
+module.exports = { getAuth: getAuth }
+
+if (require.main === module) getAuth().then(console.log);
