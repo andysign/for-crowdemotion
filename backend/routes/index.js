d55fabd9f29d87e08d61c404224b2f7454d566ce
andysign <andysignerdesigner@gmail.com>
2019-11-10 20:42:20 +0000

    Cleanup index router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET homepage. */
 router.get('/', function(req, res, next){
 	res.send('SimpleAPIbackend.\nUse /api-endpoint to call the backend');
 });
