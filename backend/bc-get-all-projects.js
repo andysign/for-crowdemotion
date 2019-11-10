b9abdcbae00c6f0a1d76fdbd24d6c39ef3266b05
andysign <andysignerdesigner@gmail.com>
2019-11-10 20:26:17 +0000

    Rewritten the bc get all projects

--- backend/bc-get-all-projects.js
+++ backend/bc-get-all-projects.js
@@ -6,30 +6,23 @@
 const request = require("request-promise"); // var request = require("request");
 const getAuthToken = require('./bc-get-auth-token');
 
-const prc = process;
-const auth = prc.argv[2] ? prc.argv[2] : '';
-const url = 'https://api.uat.pe.researchnow.com/sample/v1/projects';
-
-const options = { method: 'GET',
-	url: url,
-	qs: { limit: '10' },
-	headers:
-		{ authorization: '', // 'Bearer '+auth
-		'content-type': 'application/json' } };
-
-if (auth) {
-	options.headers.authorization = 'Bearer '+auth;
-	const call = request(options);
-	call.then( function(r) {
-		console.log(r);
-	} ).catch( function() { console.error('Call failed...'); } );
-} else {
-	getAuthToken.then(function(a){
-		options.headers.authorization = 'Bearer '+a.accessToken;
-		const call = request(options);
-		if (require.main !== module) { module.exports = call; }
-		call.then( function(r) {
-			console.log(r);
-		} ).catch( function() { console.error('Call failed...'); } );
-	}).catch( function() { console.error('Error'); });
+const getAllProjects = function () {
+	return new Promise( function (res, rej) {
+		getAuthToken.getAuth().then(function(auth){
+			const url = 'https://api.uat.pe.researchnow.com/sample/v1/projects';
+			const options = { method: 'GET',
+				url: url,
+				qs: { limit: '100' },
+				headers:
+					{ authorization: 'Bearer '+auth+'',
+					'content-type': 'application/json' } };
+			request(options).then(function(r){
+				res(r);
+			});
+		});
+	} );
 }
+
+module.exports = { getAllProjects: getAllProjects }
+
+if (require.main === module) getAllProjects().then(console.log);
