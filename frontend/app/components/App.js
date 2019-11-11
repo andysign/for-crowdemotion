657cd6f47c76dc4aa2a613ad5ac0aec2a9a16770
andysign <andysignerdesigner@gmail.com>
2019-11-11 20:42:00 +0000

    Added app css made with a special design program

--- frontend/app/components/App.js
+++ frontend/app/components/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import styles from './static/App.css';
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			authToken: null
+		}
 	}
 
 	render() {
