22f88152f8de7c0a7c680d1b944baa5c9b40d86a
andysign <andysignerdesigner@gmail.com>
2019-11-13 00:49:08 +0000

    Added ProjectForum constructor initial state

--- frontend/app/components/projects/ProjectForm.js
+++ frontend/app/components/projects/ProjectForm.js
@@ -3,13 +3,25 @@ import React from 'react';
 class ProjectForm extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {}
+		let project = this.props.project;
+		this.state = {
+			title: project.title,
+			editableLineNum: 0,
+			extLineItemId: project.lineItems[0].extLineItemId,
+			countryISOCode: project.lineItems[0].countryISOCode,
+			languageISOCode: project.lineItems[0].languageISOCode,
+			quotaPlanFilterOptions:
+				project.lineItems[0].quotaPlan.filters[0].options,
+			targetsCount: project.lineItems[0].targets[0].count
+		}
 		this.handleLastUpdateSubmit = this.handleLastUpdateSubmit.bind(this);
 	}
+
 	handleLastUpdateSubmit(event) {
 		event.preventDefault();
 		this.props.handleLastUpdate();
 	}
+
 	render () {
 		return (
 			<>
