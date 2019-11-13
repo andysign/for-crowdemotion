13e9d9349f248cad88583893fab9db0adef1b163
andysign <andysignerdesigner@gmail.com>
2019-11-13 01:34:54 +0000

    Added functionality to save the form data with sdk updateProject

--- frontend/app/components/projects/ProjectForm.js
+++ frontend/app/components/projects/ProjectForm.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import BackendSdk from './../../utils/sdk/Sdk';
 
 class ProjectForm extends React.Component {
 	constructor(props) {
 		super(props);
 		let project = this.props.project;
 		this.state = {
+			error: null,
+			extProjectId: project.extProjectId,
 			title: project.title,
 			editableLineNum: 0,
 			extLineItemId: project.lineItems[0].extLineItemId,
@@ -14,21 +17,37 @@ class ProjectForm extends React.Component {
 				project.lineItems[0].quotaPlan.filters[0].options,
 			targetsCount: project.lineItems[0].targets[0].count
 		}
-		this.handleLastUpdateSubmit = this.handleLastUpdateSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.sdk = new BackendSdk(this.props.authToken);
 	}
 
-	handleLastUpdateSubmit(event) {
+	handleChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value,
+		});
+	}
+
+	handleSubmit(event) {
 		event.preventDefault();
-		this.props.handleLastUpdate();
+		let data = {title: this.state.title}
+		this.sdk.updateProject(this.state.extProjectId, data, (response)=>{
+			if (response.isSucessful) {
+				this.props.handleLastUpdate();
+			} else {
+				this.setState({error: response.error})
+			}
+		});
 	}
 
 	render () {
 		return (
-			<form onSubmit={this.handleLastUpdateSubmit}>
+			<form onSubmit={this.handleSubmit}>
 				<div className="form-group">
 					<label>Title</label>
 					<input name="title"
 						className="form-control"
+						onChange={this.handleChange}
 						defaultValue={this.state.title}
 					/>
 				</div>
@@ -38,6 +57,7 @@ class ProjectForm extends React.Component {
 					<b>Save</b>
 				</button>
 				{this.props.lastUpdate && <h2><br />DONE</h2>}
+				{this.state.error && <h2>Error</h2>}
 			</form>
 		);
 	}
